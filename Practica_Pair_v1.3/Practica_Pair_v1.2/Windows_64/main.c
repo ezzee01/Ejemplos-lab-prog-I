40c2199ed9ff511fae276f76db113a877d03776b
ezzee01 <ezec_01@hotmail.com>
2018-06-04 22:16:18 -0300

    puntero parser practica

---
+++ Practica_Pair_v1.3/Practica_Pair_v1.2/Windows_64/main.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "ArrayList.h"
+#include "Employee.h"
+
+/****************************************************
+    Menu:
+        1. Parse del archivo data.csv
+        2. Listar Empleados
+        3. Ordenar por nombre
+        4. Agregar un elemento
+        5. Elimina un elemento
+        6. Listar Empleados (Desde/Hasta)
+*****************************************************/
+
+
+int main()
+{
+    ArrayList* listaEmpleados;
+    listaEmpleados = al_newArrayList();
+    Employee* miEmpleado;
+    Employee* aux;
+
+    miEmpleado = (Employee*) malloc(sizeof(Employee));
+    otroEmpleado = (Employee*) malloc(sizeof(Employee));
+
+    miEmpleado->id = 8;
+    strcpy(miEmpleado->name, "juan");
+    miEmpleado->isEmpty = 1;
+    strcpy(miEmpleado->lastName, "gomez");
+
+    otroEmpleado->id = 9;
+    strcpy(otroEmpleado->name, "ana");
+    otroEmpleado->isEmpty = 1;
+    strcpy(otroEmpleado->lastName, "gomez");
+
+    al_add(listaEmpleados, miEmpleado);
+    al_add(listaEmpleados,otroEmpleado);
+
+    for(int i=0; i<(al_len(listaEmpleados)); i++)
+    {
+
+    }
+
+    aux = (Employee*) al_get(listaEmpleados,0);
+
+
+
+
+
+    printf("%d--%s\n", aux->id, aux->name);
+
+    printf("%d", al_len(listaEmpleados));
+
+    return 0;
+}
